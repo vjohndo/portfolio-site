3042c2fa9f410a32706771817ab60ee34673382d
John Do <v.john.do@outlook.com>
2022-02-17 01:25:15 +1100

    page setup - todo: update HTML and Style

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 import './style.css'
 import * as THREE from 'three'
 import * as dat from 'lil-gui'
+import gsap from 'gsap'
 
 /**
  * Debug
@@ -13,6 +14,10 @@ const parameters = {
 
 gui
     .addColor(parameters, 'materialColor')
+    .onChange(() => {
+        material.color.set(parameters.materialColor)
+        particles.color.set(parameters.materialColor)
+    })
 
 /**
  * Base
@@ -24,13 +29,96 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Test cube
+ * Objects
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+
+// Texture
+const textureLoader = new THREE.TextureLoader()
+const gradientTexture = textureLoader.load('textures/gradients/3.jpg')
+gradientTexture.magFilter = THREE.NearestFilter
+
+// Material
+const material = new THREE.MeshToonMaterial({
+    color: parameters.materialColor,
+    gradientMap: gradientTexture
+})
+
+// Meshes
+
+const mesh1 = new THREE.Mesh(
+    new THREE.TorusKnotGeometry(0.7, 0.2, 100, 16),
+    material
 )
-scene.add(cube)
+const mesh2 = new THREE.Mesh(
+    new THREE.TorusGeometry(0.5, 0.2, 8, 30),
+    material
+)
+const mesh3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1.6, 0.1),
+    material
+)
+const mesh4 = new THREE.Mesh(
+    new THREE.ConeGeometry(0.8, 1.6, 32),
+    material
+)
+const mesh5 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 0.1, 1),
+    material
+)
+
+// Positions
+const objectsDistance = 4
+mesh1.position.y = -objectsDistance * 0
+mesh2.position.y = -objectsDistance * 1
+mesh3.position.y = -objectsDistance * 2
+mesh4.position.y = -objectsDistance * 3
+mesh5.position.y = -objectsDistance * 4
+
+const objectsOffset = 2
+mesh1.position.x = objectsOffset
+mesh2.position.x = -objectsOffset
+mesh3.position.x = objectsOffset
+mesh4.position.x = -objectsOffset
+mesh5.position.x = objectsOffset
+
+scene.add(mesh1, mesh2, mesh3, mesh4, mesh5)
+
+/**
+ * Particles
+ */
+// Geometry
+const particlesCount = 200
+const positions = new Float32Array(particlesCount*3)
+
+for(let i = 0; i < particlesCount; i++) {
+    positions[i * 3 + 0] = (Math.random() - 0.5) * 10
+    positions[i * 3 + 1] = objectsDistance * 0.4 - Math.random() * objectsDistance * 5
+    positions[i * 3 + 2] = (Math.random() - 0.5) * 10
+}
+
+const particlesGeometry = new THREE.BufferGeometry()
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+
+// Material
+const particlesMaterial = new THREE.PointsMaterial({
+    color: parameters.materialColor,
+    sizeAttenuation: true,
+    size: 0.03
+})
+
+// Points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
+
+// Permanent Rotation
+const sectionMeshes = [mesh1, mesh2, mesh3, mesh4, mesh5]
+
+/**
+ * Lights
+ */
+const directionalLight = new THREE.DirectionalLight('#ffffff', 1)
+directionalLight.position.set(1, 1, 0)
+scene.add(directionalLight)
 
 /**
  * Sizes
@@ -58,28 +146,91 @@ window.addEventListener('resize', () =>
 /**
  * Camera
  */
+// Group
+const cameraGroup = new THREE.Group()
+scene.add(cameraGroup)
+
 // Base camera
 const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 6
-scene.add(camera)
+cameraGroup.add(camera)
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    alpha: true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
+ * Scroll
+ */
+let scrollY = window.scrollY
+let currentSection = 0 
+
+window.addEventListener('scroll', () => 
+{
+    scrollY = window.scrollY
+    const newSection = Math.round(scrollY / sizes.height)
+    console.log(currentSection)
+    if (newSection != currentSection) 
+    {
+        currentSection = newSection
+        gsap.to(
+            sectionMeshes[currentSection].rotation,
+            {
+                duration: 1.5,
+                ease: 'power2.inOut',
+                x: '+=6',
+                y: '+=6',
+                z: '+=1.5'
+            }
+        )
+    }
+})
+
+/**
+ * Cursor
+ */
+const cursor = {}
+cursor.x = 0
+cursor.y = 0
+
+window.addEventListener('mousemove', (event) => 
+{
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = event.clientY / sizes.height - 0.5
+})
+
+/**
  * Animate
  */
 const clock = new THREE.Clock()
+let previousTime = 0
 
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - previousTime
+    previousTime = elapsedTime
+    
+    // Animate camera based on the scroll
+    camera.position.y = -scrollY / sizes.height * objectsDistance
+
+    const parrallaxX = cursor.x * 0.5
+    const parrallaxY = - cursor.y * 0.5
+    cameraGroup.position.x += (parrallaxX - cameraGroup.position.x) * 2 * deltaTime
+    cameraGroup.position.y += (parrallaxY - cameraGroup.position.y) * 2 * deltaTime
+
+    // Animate meshes
+    for (const mesh of sectionMeshes) 
+    {
+        mesh.rotation.x += deltaTime * 0.1
+        mesh.rotation.y += deltaTime * 0.12
+    }
 
     // Render
     renderer.render(scene, camera)
